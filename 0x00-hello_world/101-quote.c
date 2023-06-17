7c8006b6b69b5e500f8bd9535a61e684047d8cd3
Francis Emmanuel <francis.a.emmanuel@gmail.com>
2023-06-16 21:31:15 -0700

    100-QUOTE.C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - A c program that prints a line to the standard error
+ * Return 1 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	fput("and that place of art is useful\" - - Dora Korpar, 2015-10-19\n", stdout);
+	return(1);
 }
