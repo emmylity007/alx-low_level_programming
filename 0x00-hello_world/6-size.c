8384842a76b1335d2af0b8efdfa323b92f72a06b
Francis Emmanuel <francis.a.emmanuel@gmail.com>
2023-06-16 20:44:46 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,17 +3,17 @@
  *  main - Program toprint the size of various types on the computer
  *  Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
 char a;
 int b;
-long int c
+long int c;
 long long int d;
 float f;
 printf("Size of Char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of Int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of Long Int: %lu bytes(s)\n", (unsigned long)sizeof(c));
-printf("Size of Long Long Int: %lu bytes(s)\n",(unsigned long)sizeof(d));
+printf("Size of Long Long Int: %lu bytes(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
