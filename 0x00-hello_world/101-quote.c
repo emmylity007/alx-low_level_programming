a2125d9d155f0000252aef74c928f09da3db3838
Francis Emmanuel <francis.a.emmanuel@gmail.com>
2023-06-18 07:56:53 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,9 @@
 #include <unistd.h>
 /**
  * main - Entry point
-  * Description: 'the program prints line to the standard error'
- * @parameter: describe the parameter
- *
+ * Description: 'the program prints line to the standard error'
  * Return 1;(Success)
- */
+*/
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
