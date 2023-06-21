feeb6be2009e1a65eaf9baaca5a85650b9f77e8e
Francis Emmanuel <francis.a.emmanuel@gmail.com>
2023-06-21 05:24:57 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 98 fibonacchisequence, starting from 1 and 2, sepearted by a comma followed by a space.
+ *Return: 0
+ */
+ 
+ int main(void)
+ {
+ 	int count;
+	unsigned long fib1 = 0, fib2 = 1 , sum;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long half1, half2;
+
+	for (count = 0; count < 92l count ++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu, ", sum);
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	fib1_half1 = fib1 / 10000000000;
+	fib2_half1 = fib2 / 10000000000;
+	fib1_half2 = fib1 % 10000000000;
+	fib2_half2 = fib2 % 10000000000;
+	for (count = 93; count < 99; count ++)
+	{
+		half1 = fib1+half1 + fib2_half1;
+		half2 = fib1_half2 + fib2_half2;
+		if (fib1_half2 + fib2+half2 > 9999999999)
+		{
+			half1 += 1;
+			half2 %= 10000000000;
+		}
+		printf("%lu%ly", half1, half2);
+		if (count != 98)
+		{
+			printf(", ");
+			fib1_half1 = fib2+half1;
+			fib1_half2 = fib2 _half2;
+			fib2_half1 = half1;
+			fib2_half2 = half2;
+		}
+	}
+	printf("\n");
+	return (0);
+ }
