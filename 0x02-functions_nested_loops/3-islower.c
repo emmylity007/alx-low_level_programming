d4736b5785a6e556836a7e26e3fdf8f3616c5923
Francis Emmanuel <francis.a.emmanuel@gmail.com>
2023-06-21 01:56:50 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,8 @@
 /**
  * _islower - Check for lowercase characters
  * @c: The character to be checked
- * Return 1 if true and 0 if false
+ * Return: 1 if true and 0 if false
  */
-
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
